5b4417b8889b3f17011cbc85b81f779fe8862033
massone99 <lorenzomassone@gmail.com>
2023-03-29 13:14:57 +0200

    Finished exercise 2 Warehouse.scala

--- src/main/scala/u04lab/code/Warehouse.scala
+++ src/main/scala/u04lab/code/Warehouse.scala
@@ -7,10 +7,12 @@ trait Item {
 }
 
 object Item:
-  def apply(code: Int, name: String, tags: List[String] = List.empty): Item =
-    ItemImpl(code, name, tags)
+  def apply(code: Int, name: String, tags: String*): Item =
+    var tagList = List.empty[String]
+    for tag <- tags do tagList = append(tagList, cons(tag, empty))
+    ItemImpl(code, name, tagList)
 
-  case class ItemImpl(code: Int, name: String, tags: List[String]) extends Item
+  private case class ItemImpl(code: Int, name: String, tags: List[String]) extends Item
 
 /**
  * A warehouse is a place where items are stored.
@@ -68,9 +70,9 @@ object Warehouse {
 @main def mainWarehouse(): Unit =
   val warehouse = Warehouse()
 
-  val dellXps = Item(33, "Dell XPS 15", cons("notebook", empty))
-  val dellInspiron = Item(34, "Dell Inspiron 13", cons("notebook", empty))
-  val xiaomiMoped = Item(35, "Xiaomi S1", cons("moped", cons("mobility", empty)))
+  val dellXps = Item(33, "Dell XPS 15", "notebook")
+  val dellInspiron = Item(34, "Dell Inspiron 13", "notebook")
+  val xiaomiMoped = Item(35, "Xiaomi S1", "moped", "mobility")
 
   println(warehouse.contains(dellXps.code)) // false
   warehouse.store(dellXps) // side effect, add dell xps to the warehouse
